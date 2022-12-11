f53f36ed88abbb7c0b7a20705ed89332c231cb64
Mahmoud-Alaa <soluman2297@gmail.com>
2022-12-11 07:25:21 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,6 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-    //   target: 'http://localhost:3000',
-    // target : 'http://localhost:5500',
-    // target : 'https://4all.ltd/services/api/ads/companies?page_count=5',
     target : 'https://4all.ltd',
       changeOrigin: true,
     })
